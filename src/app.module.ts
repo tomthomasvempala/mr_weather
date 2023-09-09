dc649f619f1346fe9b39d6e485b8fb7884861822
Tom Vempala <thommuvtt123@gmail.com>
2023-09-09 19:45:50 +0530

    idk again

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegramModule } from './telegram/telegram.module';
+import { TelegramService } from './telegram/telegram.service';
 
 @Module({
   imports: [TelegramModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,TelegramService],
 })
 export class AppModule {}
